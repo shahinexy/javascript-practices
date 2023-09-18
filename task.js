06b0f94d5f5e5720ea2dc7a3033e9262d3fc3cf8
shahinexy <138812841+shahinexy@users.noreply.github.com>
2023-09-18 23:59:45 +0600

    Add files via upload

---
+++ task.js
@@ -0,0 +1,51 @@
+// Task 10
+let x = 3984567;
+console.log(x.toString())
+
+// Task 11
+let x1 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit harum eveniet laborum sunt obcaecati, tempora iusto officiis doloribus exercitationem doloremque minima. Debitis ipsam dolorem non. Nihil labore tenetur unde quas.';
+console.log(x1.slice(0,20))
+
+//task 12
+let x2 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit';
+console.log(x2.length)
+
+//task 13
+let x3 = 3.1416;
+console.log(x3.toFixed(2));
+
+//task 14
+let Name = 'Johon Done';
+let Age = 23;
+let GPA = 4.75;
+console.log(`Name:${Name} 
+Age:${Age} 
+GPA:${GPA}`);
+
+//TASK 15
+let x5 = "234";
+console.log(parseInt(x5));
+
+//Task 16
+let x6 = 18423;
+console.log(isNaN(x6));
+
+// Task 17
+let x8 = '87.453';
+console.log(parseFloat(x8));
+
+// Task 18
+var myText = 'The quick brown fox has jumped';
+console.log(myText.includes('fox'));
+
+// Task 19
+let padded = '199';
+console.log(padded.padStart(8,"0"));
+
+// Task 20
+let recipientName = 'Rahat' ;
+let senderName = 'Shahin';
+let customMessage = 'How are you Rahat vi.?';
+console.log(`Resipient Name:${recipientName} 
+${customMessage}
+Sender Name:${senderName}`);
